29fd9a9ee01f8d0aba6a72de0d40f6bd001ac5bb
Amitjaison31 <amitrihandj@gmail.com>
2023-10-02 13:04:51 +0530

    Update

--- app/src/main/java/com/example/bartersystem/OnBoardingActivity.java
+++ app/src/main/java/com/example/bartersystem/OnBoardingActivity.java
@@ -4,27 +4,83 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
 
     ViewPager viewPager;
     LinearLayout dotsLayout;
+    Button btn;
     SliderAdapter sliderAdapter;
 
+    TextView[] dots;
+    private int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
+
+
 
         viewPager = findViewById(R.id.slider);
         dotsLayout = findViewById(R.id.dots);
+        btn = findViewById(R.id.get_started_btn);
+
+        addDots(position);
+        viewPager.addOnPageChangeListener(changeListener);
 
         //Call Adapter
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
 
     }
+
+    private void addDots(int position){
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+        for (int i = 0; i<dots.length; i++){
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[this.position].setTextColor(getResources().getColor(R.color.darkblue));
+        }
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            if (position == 0){
+                btn.setVisibility(View.INVISIBLE);
+            } else if (position == 1) {
+                btn.setVisibility(View.INVISIBLE);
+            } else {
+                btn.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
